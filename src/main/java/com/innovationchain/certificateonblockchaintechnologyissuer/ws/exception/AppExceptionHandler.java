3b4ac0c62f8d3db37c3172679efeb1d04737b37b
jimsa <cse.isalehi@gmail.com>
2022-11-22 14:49:56 +0330

    implement connection Exception handling

---
+++ src/main/java/com/innovationchain/certificateonblockchaintechnologyissuer/ws/exception/AppExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.innovationchain.certificateonblockchaintechnologyissuer.ws.exception;
+
+import com.innovationchain.certificateonblockchaintechnologyissuer.ws.ui.model.response.ErrorMessage;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
+
+@ControllerAdvice
+public class AppExceptionHandler {
+
+    // handle a specific exception
+    @ExceptionHandler(value = {ConnectionServiceException.class})
+    public ResponseEntity<Object> handleUserServiceException(ConnectionServiceException connectionServiceException, WebRequest request) {
+        ErrorMessage errorMessage = new ErrorMessage(new Date(), connectionServiceException.getMessage());
+        return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    // handle all exceptions
+    @ExceptionHandler(value = {Exception.class})
+    public ResponseEntity<Object> handleUserServiceException(Exception exception, WebRequest request) {
+        ErrorMessage errorMessage = new ErrorMessage(new Date(), exception.getMessage());
+        return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
---
+++ src/main/java/com/innovationchain/certificateonblockchaintechnologyissuer/ws/exception/ConnectionServiceException.java
@@ -0,0 +1,7 @@
+package com.innovationchain.certificateonblockchaintechnologyissuer.ws.exception;
+
+public class ConnectionServiceException extends  RuntimeException{
+    public ConnectionServiceException(String exception) {
+        super(exception);
+    }
+}
