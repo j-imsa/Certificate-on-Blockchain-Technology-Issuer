c16fc92f697d2c5367dc01d43ebe88c8ced8ea7a
jimsa <cse.isalehi@gmail.com>
2022-11-22 14:47:05 +0330

    create Request model url

---
+++ src/main/java/com/innovationchain/certificateonblockchaintechnologyissuer/ws/ui/model/request/ConnectionUrl.java
@@ -0,0 +1,14 @@
+package com.innovationchain.certificateonblockchaintechnologyissuer.ws.ui.model.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class ConnectionUrl {
+    private String url;
+}
